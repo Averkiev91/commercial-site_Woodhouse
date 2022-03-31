be230e92f9bca1881df641c85bcf60d8813121a3
Averkiev91 <amd3500m@gmail.com>
2022-03-31 17:55:47 +0300

    slider

--- js/common.js
+++ js/common.js
@@ -1,24 +1,90 @@
-document.addEventListener('DOMContentLoaded', function() {
-// Menu
-	const toggleMenu = document.querySelector('.menu-toggle')
-	const topMenu = document.querySelector('.top-menu')
+document.addEventListener('DOMContentLoaded', function () {
 
-	toggleMenu.addEventListener('click', () => {
-		topMenu.classList.toggle('top-menu_active')
-		toggleMenu.classList.toggle('menu-toggle_active')
+	MicroModal.init({
+		openTrigger: 'data-custom-open',
+		closeTrigger: 'data-custom-close',
+		disableScroll: true,
+		disableFocus: true,
+		awaitOpenAnimation: true,
+		awaitCloseAnimation: true
 	})
-	document.addEventListener('click', (e) => {
-		if (e.target.closest('.menu-container')) return
-		toggleMenu.classList.remove('menu-toggle_active')
-		topMenu.classList.remove('top-menu_active')
+	$('[data-custom-open]').click(function () {
+		$('.modal [name=form]').val($(this).data('form'))
+	})
+	$('[data-custom-close]').click(function () {
+		$('.modal [name=form]').val('')
 	})
 
-//Swiper Sliders
+	$('.home-slider__heading').each(function () {
+		let text = $(this).text().split(' '),
+			first = text.shift()
+		$(this).html(`${first} <br><span>${text.join(' ')}</span>`)
+	})
+
+	// Menu
+
+	function toggleMenu() {
+		$('.menu-toggle').toggleClass('menu-toggle_active')
+		$('.top-menu').toggleClass('top-menu_active')
+	}
+
+	$('.menu-toggle').click(function () {
+		toggleMenu()
+	})
+
+	function closeMenu() {
+		$('.menu-toggle').removeClass('menu-toggle_active')
+		$('.top-menu').removeClass('top-menu_active')
+	}
+
+	$(document).click(function (e) {
+		if ($(e.target).closest('.menu-container').length) return
+		closeMenu()
+	})
+
+	// Swiper Sliders
 
 	const homeSlider = new Swiper('.home-slider', {
 		speed: 800,
 		effect: 'fade',
-		ceneredSlides: true,
+		centeredSlides: true,
+		pagination: {
+			el: '.home-slider__pagination',
+			type: 'custom',
+			renderCustom: function (swiper, current, total) {
+				let indT = total >= 10 ? total : `0${total}`
+				let indC = current >= 10 ? current : `0${current}`
+				return `<b>${indC}</b><span></span> ${indT}`
+			}
+		},
+		scrollbar: {
+			el: '.home-siler__scrollbar',
+			draggable: true
+		},
+		navigation: {
+			prevEl: '.home-slider__prev',
+			nextEl: '.home-slider__next'
+		},
+		keyboard: {
+			enabled: true,
+			onlyInViewport: false
+		},
+		runCallbacksOnInit: true
+	})
+
+	const advSlider = new Swiper('.advantages-slider', {
+		effect: 'fade',
+		speed: 1400,
+		autoplay: {
+			delay: 5000,
+			disableOnInteraction: false
+		}
+	})
+
+	// Callback List Numbers
+
+	$('.home-callback__list li').each(function () {
+		$(this).html($(this).text().replace(/([0-9.]+)/g, '<span>$1</span>'))
 	})
 
 })
